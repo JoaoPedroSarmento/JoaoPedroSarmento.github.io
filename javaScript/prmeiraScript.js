962984f7f20b49702ba9fa809fc35dd26fa64e4b
João Pedro Dieguez Moraes Sarmento <103864422+joaosarmento12345@users.noreply.github.com>
2022-07-09 17:48:57 -0300

    Add files via upload

---
+++ javaScript/prmeiraScript.js
@@ -0,0 +1,7 @@
+alert("Bem-vindo A seguir pediremos que informe alguns dados ");
+let nome = prompt("Informe seu nome");
+let idade = prompt("Informe sua idade ");
+let confirmado = confirm("Sua idade é " + idade + " anos");
+alert(
+  "Nome :" + nome + "\nIdade : " + idade + "\nIdade confirmada : " + confirmado
+);
