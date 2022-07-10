01df8ed6dba882510cb8d70ae2dff3d8d676bc02
João Pedro Dieguez Moraes Sarmento <103864422+joaosarmento12345@users.noreply.github.com>
2022-07-09 23:32:44 -0300

    Update segundaScript.js

--- javaScript/segundaScript.js
+++ javaScript/segundaScript.js
@@ -1,10 +1,12 @@
 alert("Iremos pedir alguns dados a seguir\n\n\n");
 let old = prompt("Digite o nome  da pessoa mais velha\n");
 let old2 = prompt("Digite a idade da pessoa mais velha\n");
-
 let new1 = prompt("Digite o nome  da pessoa mais nova\n");
 let new2 = prompt("Digite a idade da pessoa mais nova\n");
 let idade = old2 - new2;
+if (new2 == old2 || old2 == new2) {
+  alert("A idade e a mesma");
+}
 
 alert(
   "O nome da pessoa mais velha e : " +
@@ -14,8 +16,21 @@ alert(
     "\nO nome da pessoa mais nova e : " +
     new1 +
     " \ne  sua idade é : " +
-    new2 +
-    "\nA diferença de idade é de  : " +
-    idade +
-    "ano[s]"
+    new2
 );
+
+if (new2 != old2 || old2 != new2) {
+  alert(
+    "O nome da pessoa mais velha e : " +
+      old +
+      "\n e  sua idade é : " +
+      old2 +
+      "\nO nome da pessoa mais nova e : " +
+      new1 +
+      " \ne  sua idade é : " +
+      new2 +
+      "\nA diferença de idade é de  : " +
+      idade +
+      "ano[s]"
+  );
+}
