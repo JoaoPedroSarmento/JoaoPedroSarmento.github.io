631ad664dc7389a86ea31ae886c431cf864521d1
João Pedro Dieguez Moraes Sarmento <103864422+joaosarmento12345@users.noreply.github.com>
2022-07-12 22:35:14 -0300

    2022

---
+++ c ++/provas/prr1.cpp
@@ -0,0 +1,52 @@
+#include<iostream>//bibiotleca que inclui o cout
+#include<conio.h>//inclui o getch() que faz com que, quando o programa for executado ele n feche
+
+
+using namespace std;/* para usar os recursos de entrada e saída da biblioteca iostream em C++, é preciso incluir o comando using namespace std;
+ Este comando serve para definir um "espaço de nomes", ou namespace.*/
+ 
+ void piscina(float &c,float &l , float p)
+ {
+ 	float area;
+ 	float volume;
+ 	
+ 	area =  c * l;
+ 	volume = area * p;
+ 	
+ 	
+ 	c = area;
+ 	
+ 	l = volume ;
+ 	
+ 	
+ }
+
+
+int main()
+{
+	float C,L,P;
+	
+	cout<<"Digite o valor do comprimento  da piscina\n";
+	cin>>C;
+	
+	cout<<"Digite o valor da largura da piscina\n";
+	cin>>L;
+	
+	cout<<"Digite o valor da profundidade da piscina\n";
+	cin>>P;
+	
+	piscina(C,L,P);
+	
+	cout<<"O valor da area  e equivalente a : "<<C<<"\n";
+	
+	cout<<"O valor do volume da piscina e equivalente a : "<<L<<"\n";
+	
+	getch();
+	
+	
+	
+	
+	
+	
+}
+
