36a8d54bcddcb2637d642a8a597203d4a39f8910
João Pedro Dieguez Moraes Sarmento <103864422+joaosarmento12345@users.noreply.github.com>
2022-07-15 23:21:56 -0300

    c++

---
+++ c ++/LISTA 4/exer7.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+
+int main()
+
+{
+	char L;
+	cout<<"Digite uma letra\n";
+	cin>>L;
+	
+	switch(L)
+	{
+		case 'a':
+			case'A':
+				cout<<"A letra "<<L<<" esta na posicao 1 do alfabeto ";
+		break;
+			case 'b':
+			case'B':
+				cout<<"A letra "<<L<<" esta na posicao 2 do alfabeto ";
+		break;
+			case 'c':
+			case'C':
+				cout<<"A letra "<<L<<" esta na posicao 3 do alfabeto ";
+		break;
+			case 'd':
+			case'D':
+				cout<<"A letra "<<L<<" esta na posicao 4 do alfabeto ";
+		break;
+		
+			case 'E':
+			case'e':
+				cout<<"A letra "<<L<<" esta na posicao 5 do alfabeto ";
+		break;
+		
+			case 'f':
+			case'F':
+				cout<<"A letra "<<L<<" esta na posicao 6 do alfabeto ";
+		break;
+			case 'g':
+			case'G':
+				cout<<"A letra "<<L<<" esta na posicao 7 do alfabeto ";
+		break;
+			case 'h':
+			case'H':
+				cout<<"A letra "<<L<<" esta na posicao 8 do alfabeto ";
+		break;
+		
+			case 'i':
+			case'I':
+				cout<<"A letra "<<L<<" esta na posicao 9 do alfabeto ";
+		break;
+			case 'j':
+			case'J':
+				cout<<"A letra "<<L<<" esta na posicao 10 do alfabeto ";
+		break;
+			case 'k':
+			case'K':
+				cout<<"A letra "<<L<<" esta na posicao 11 do alfabeto ";
+		break;
+	
+		case 'l':
+			case'L':
+				cout<<"A letra "<<L<<" esta na posicao 12 do alfabeto ";
+		break;
+		
+			case 'm':
+			case'M':
+				cout<<"A letra "<<L<<" esta na posicao 13 do alfabeto ";
+		break;
+			case 'n':
+			case'N':
+				cout<<"A letra "<<L<<" esta na posicao 14 do alfabeto ";
+		break;
+		case 'o':
+			case'O':
+				cout<<"A letra "<<L<<" esta na posicao 15 do alfabeto ";
+		break;
+		case 'p':
+			case'P':
+				cout<<"A letra "<<L<<" esta na posicao 16 do alfabeto ";
+		break;
+	
+		case 'q':
+			case'Q':
+				cout<<"A letra "<<L<<" esta na posicao 17 do alfabeto ";
+		break;
+			case 'r':
+			case'R':
+				cout<<"A letra "<<L<<" esta na posicao 18 do alfabeto ";
+		break;
+		case 's':
+			case'S':
+				cout<<"A letra "<<L<<" esta na posicao 19 do alfabeto ";
+		break;
+	
+		case 't':
+			case'T':
+				cout<<"A letra "<<L<<" esta na posicao 20 do alfabeto ";
+		break;
+		
+			case 'u':
+			case'U':
+				cout<<"A letra "<<L<<" esta na posicao 21 do alfabeto ";
+		break;
+			case 'v':
+			case'V':
+				cout<<"A letra "<<L<<" esta na posicao 22 do alfabeto ";
+		break;
+		case 'w':
+			case'W':
+				cout<<"A letra "<<L<<" esta na posicao 23 do alfabeto ";
+		break;
+			case 'x':
+			case'X':
+				cout<<"A letra "<<L<<" esta na posicao 24 do alfabeto ";
+		break;
+			case 'y':
+			case'Y':
+				cout<<"A letra "<<L<<" esta na posicao 25 do alfabeto ";
+		break;
+		case 'z':
+			case'Z':
+				cout<<"A letra "<<L<<" esta na posicao 26 do alfabeto ";
+		break;
+	default :
+		cout<<"Isso nao e uma letra\n";
+	
+	}
+	
+	
+	
+	
+}
