6da4ebecefdc6b1b061eb7169693c701617ace52
João Pedro Dieguez Moraes Sarmento <103864422+joaosarmento12345@users.noreply.github.com>
2022-07-17 15:42:07 -0300

    EcmasScript

--- javaScript/exer  curso/exerwhile.js
+++ javaScript/exer  curso/exerwhile.js
@@ -1,20 +1,23 @@
-//terminar exercicío
 let nave = prompt("Digite o  nome da nave\n");
-
+    
 let dobra = prompt("Deseja entrar na dobra ?");
 
 let dobras = 0;
 
-if (dobra == "sim" || dobra == "s" || dobra == "SIM" || dobra == "Sim") {
-  dobras++;
-  if (dobra == "sim" || dobra == "s" || dobra == "SIM" || dobra == "Sim") {
-    while (dobra == "sim" || dobra == "s" || dobra == "SIM" || dobra == "Sim") {
+  if (dobra == "sim" || dobra == "s" || dobra == "SIM" || dobra == "Sim") 
+       {//1
+   dobras++;
+  if (dobra == "sim" || dobra == "s" || dobra == "SIM" || dobra == "Sim") 
+ {//2
+   while (dobra == "sim" || dobra == "s" || dobra == "SIM" || dobra == "Sim") 
+   {//3
       dobra = prompt("Deseja entrar na dobra (novamente) ?");
       dobras++;
-    }
-  }
-} else if (dobra == "nao" || dobra == "n") {
+   }//3
+ }//2
+        }//1
+  else if (dobra == "nao" || dobra == "n") {
   alert("O nome  da nave é : " + nave + " dobra[s] = " + dobras);
-}
+  }
 
-alert("O nome  da nave é : " + nave + " dobra[s] =   " + dobras);
+  alert("O nome  da nave é : " + nave + " dobra[s] =   " + dobras);
