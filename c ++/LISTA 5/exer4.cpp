4bcd0fa7b3aa03aad1f7fb7876941435473c0dbf
João Pedro Dieguez Moraes Sarmento <103864422+joaosarmento12345@users.noreply.github.com>
2022-08-06 10:57:32 -0300

    c++

---
+++ c ++/LISTA 5/exer4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+ 
+ int Fatorial(int n)
+ {
+  int i, fatorial = 1 ;
+  for(i = 1  ; i <= n ; i++ )
+  {
+  fatorial = fatorial * i;
+  }
+  return fatorial;
+ }
+
+int main()
+{
+	 int n, resultado;
+	cout<<"Digite um numero ---> ";
+	cin>>n;
+	resultado = Fatorial(n);
+	cout<<"O fatorial de ----> "<<n<<" e ---> "<<resultado;
+    getch();
+}
