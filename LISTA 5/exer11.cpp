36b8dd1423bae285da06c77b7769ff5959084085
João Pedro Dieguez Moraes Sarmento <103864422+joaosarmento12345@users.noreply.github.com>
2022-08-13 10:09:55 -0300

    Exercício n-11

---
+++ LISTA 5/exer11.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int r;
+	int n;
+    cout<<"Digite um numero inteiro --- > ";
+    cin>>n;
+    int c =  sqrt(n);
+    int result = c * c ;
+	cout << "O maior quadrado menor ou igual a " << n << " e -->" <<result<<"\n";
+	cout<<"Deseja digitar mais quantos numeros ?";
+	cin>>r;
+	for(int i = 1; i < r; i++)
+	{
+	cout<<"Informe um numero inteiro --- > \n\n";
+    cin>>n;
+	cout << "O maior quadrado menor ou igual a " << n << " e -->" <<result<<"\n\n";
+	
+	}
+  
+    getch();
+}
