79170c3b7372067d3bef5df7d1e5f60d23b86532
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-08-29 20:18:58 -0300

    c++

--- c ++/LISTA 4/exer5.cpp
+++ c ++/LISTA 4/exer5.cpp
@@ -3,37 +3,21 @@
 
 using namespace std;
 
-int triangulo(int l1,int l2, int l3)
+int TipoDoTriangulo(int l1,int l2, int l3)
 {
-	// l1 + l2 || l1 + l3 || l2 + l3
-	int soma1 = l1 + l2;
-	int soma2 = l1 + l3;
-	int soma3 = l2 + l3;
+	int retorno;
 	
-	if ( l1 >  soma1 ||  l1  > soma2 || l1 > soma3 || l2 >  soma1 ||  l2  > soma2 || l2 > soma3 ||l3 >  soma1 ||  l3  > soma2 || l3 > soma3 )
-	{
-		return 0;
-	}
-	
-	else
-	{
-		if(l1 == l2 && l1 == l3 && l2 == l3) 	return 1;
-		
-		
-		
-	}
 
-     
-     
-	 
-	    if(l1 == l2 && l1 != l3  || l1 == l3 && l1 != l2 || l2 == l3 && l2 != l1) return 2;
+  (l1 == l2 && l1 == l3 && l2 == l3) ? 	retorno =   1 : retorno; ;
+  
+  (l1 == l2 && l1 != l3  || l1 == l3 && l1 != l2 || l2 == l3 && l2 != l1) ?  retorno =  2 : retorno;  
+  
+  (l1 != l2 && l1 != l3 && l2 != l3) ? retorno = 3 : retorno; 
+  
+  (l1 + l2 < l3 ) ?  retorno = 0 : retorno; 
 
-    	
-    
-    else
-    return 3;
+  return retorno;
 }
-
 
 int main()
 
@@ -48,24 +32,22 @@ int main()
 	
 	cout<<"Digite o terceiro lado de um triangulo\n";
 	cin>>l3;
-	switch(triangulo( l1, l2, l3))
+	switch(TipoDoTriangulo( l1, l2, l3))
 	{
 		case 0 : 
 		cout<<"Nao e um triangulo\n";
 		break;
 		case 1 : 
-		cout<<"Triangulo escale no\n";
+		cout<<"Triangulo escaleno\n";
 		break;
 		case 2 :
 	    cout<<"Triangulo isosceles";
 	    break;
+
 	    default :
 	    	cout<<"triangulo equilatero";
-		
+	    	break;
 	}
-	
 	getch();
 	return 0;
-	
-	
 }
