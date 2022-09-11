4dcc9642f3857b8aca2fa9720d50e7b3ddcf4bd4
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-09-11 17:44:15 -0300

    Add files via upload

---
+++ Site/index.js
@@ -0,0 +1,7 @@
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+  console.log("CLICOU NO MENU");
+}
+menuOnClick();
