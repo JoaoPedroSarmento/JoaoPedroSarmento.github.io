b477764ef4229252b06f0ddcc218456fe7b84fc9
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-10-18 11:55:29 -0300

    Delete python/LISTA 1 directory

--- python/LISTA 1/exer1.py
+++
@@ -1,6 +0,0 @@
-
-nome = input("Digite seu nome :")
-
-print('Seu nome é :  ' + nome )
-
-
--- python/LISTA 1/exer3.py
+++
@@ -1,5 +0,0 @@
-nome = input('Digite seu primeiro nome')
-
-sobrenome = input ('Digite seu sobrenome ')
-
-print(sobrenome + "," + nome)
