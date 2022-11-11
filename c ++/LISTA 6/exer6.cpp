ab7ebbb153180c2ad3201c03ba4d92d86e7e39fb
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-11-10 23:08:13 -0300

    c++

--- c ++/LISTA 6/exer6.cpp
+++ c ++/LISTA 6/exer6.cpp
@@ -4,42 +4,41 @@ using namespace std;
 
 int InverteVetor(int v[] , int tam )
 {
-	 int j = 0 ;
-	 int aux;
-	 int tam2 = tam/2; 
-	 
-	 for ( int i = tam - 1 ; tam2 <  i ; i-- )  
-	  {
-	    	aux = v[j];
-	    	v[j] = v[i];
-	    	v[i] = aux;
-	        j++	;
-	    
-	  }
-	  
+     int aux;
+     int i = 0 ;
+        for ( int j = tam - 1  ; j  >   i  ; j-- ) {
+
+            aux = v[j] ;
+            v[j] = v[i] ;
+            v[i] = aux ;
+            i++;
+
+        }
+
 }
+
 
 int main ()
 {
 	int tam , valor ;
 	cout << "Digite o tamanho do vetor -- > ";
 	cin >> tam ;
-	
+
 	int v[tam];
-	
+
    for ( int i = 0 ; i < tam ; i ++ )
    {
    	  cout << "Insira o valor do vetor , posicao -- > " << i;
    	  cin >> valor;
 	  v[i] = valor;
    }
-	
+
 	InverteVetor(v, tam );
-	
+
 	 for ( int i = 0 ; i < tam ; i++)
      {
-     	cout << "Vetor , posicao -- > " << i << "\tvalor -- > " << v[i] << endl ; 
+     	cout << "Vetor , posicao -- > " << i << "\tvalor -- > " << v[i] << endl ;
 	 }
 	 getch();
- 
+
 }
