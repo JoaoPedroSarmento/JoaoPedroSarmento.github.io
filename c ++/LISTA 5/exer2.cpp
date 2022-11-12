16670b7beb11b688697aa07db9c644fdfe325341
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-11-12 15:07:26 -0300

    Update exer2.cpp

--- c ++/LISTA 5/exer2.cpp
+++ c ++/LISTA 5/exer2.cpp
@@ -2,50 +2,48 @@
 
 using namespace std ;
 
-int potencia( int n , int p) 
+int potencia( int n , int p)
 {
-	int ResultadoDaPotencia = 1;  
-    if(p >= 1 )  
+	int ResultadoDaPotencia = 1;
+    if(p >= 1 )
     {
-     
-    	
-    	for(int i = 1; i <= p ; i++  )  
+
+
+    	for(int i = 1; i <= p ; i++  )
     	{
-    		ResultadoDaPotencia = ResultadoDaPotencia * n; 
-		} 
-		return ResultadoDaPotencia; 
+    		ResultadoDaPotencia = ResultadoDaPotencia * n;
 	}
+		return ResultadoDaPotencia;
+    }
+
+      
 	else
-	if( p < 0 )
-	{
+	if( p < 0 ) {
+
+
+    	for(int i = -1; i >= p ; i--) { 
 		
-    	
-    	for(int i = -1; i >= p ; i--)  
-    	{
-    		ResultadoDaPotencia = ResultadoDaPotencia * n; 
-		} 
-		
-		  if(p % 2  == 0) 		
-	      return ResultadoDaPotencia; 
-	      
-	      return ResultadoDaPotencia * -1;
+		ResultadoDaPotencia = ResultadoDaPotencia * n;
+		}
+
+		return (p % 2 == 0 ) ? ResultadoDaPotencia : ResultadoDaPotencia * -1;
 	}
-	
-	
+
+
  	return 1;
 
-}
+  }
 
-	
+
 int main()
 {
 	int n , p ;
-    
+
     cout << "Escreva um numero -- > ";
     cin >> n ;
-    
+
     cout << "Escreva a potencia -- > ";
     cin >> p ;
-    
+
     cout << "O resultado da potencia e -- > " <<potencia(n,p);
 }
