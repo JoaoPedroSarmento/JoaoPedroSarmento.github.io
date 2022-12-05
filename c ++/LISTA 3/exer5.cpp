93ea0b8d3a505832b7b4a391212ae4bf67e877a8
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-12-05 11:25:57 -0300

    Update exer5.cpp

--- c ++/LISTA 3/exer5.cpp
+++ c ++/LISTA 3/exer5.cpp
@@ -4,26 +4,27 @@
 
 using namespace std;
 
-void retorno (float c, float l, float p, float &agua, float &piso)
-{
-float volume, area;
-volume = p * c * l;
-area = c * l;
-agua = volume;
-piso = area;
+void retorno (float c, float l, float p, float &agua, float &piso) { 
+  agua  = p * c * l;
+  piso  = c * l;
 }
 
-int main()
-{
-float comprimento, largura, profundidade,  v, a;
-cout << "Digite o comprimento da piscina: \n";
-cin >> comprimento;
-cout << "Digite a largura da piscina: \n";
-cin >> largura;
-cout << "Digite a profundidade da piscina: \n";
-cin >> profundidade;
-retorno (comprimento, largura, profundidade, v, a);
-cout << "O volume de agua armazenado pela piscina vale: " << v << "\n";
-cout << "A area de piso a ser substituido vale: " << a;
-getch ();
+int main() { 
+    
+    float comprimento, largura, profundidade,  v, a;
+    cout << "Digite o comprimento da piscina: \n";
+    
+    cin >> comprimento;
+    cout << "Digite a largura da piscina: \n";
+    
+    cin >> largura;
+    cout << "Digite a profundidade da piscina: \n";
+    
+    cin >> profundidade;
+    retorno (comprimento, largura, profundidade, v, a);
+    
+    cout << "O volume de agua armazenado pela piscina vale: " << v << "\n";
+    cout << "A area de piso a ser substituido vale: " << a;
+    
+    getch ();
 }
