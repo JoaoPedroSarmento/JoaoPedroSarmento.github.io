f7a1296fbb6eb4afda92aee4f84ecf5ac2d85437
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-12-05 10:54:53 -0300

    Update exer1.cpp

--- c ++/LISTA 3/exer1.cpp
+++ c ++/LISTA 3/exer1.cpp
@@ -4,48 +4,33 @@
  
  using namespace std;
  
- void retorno ( float &xi,float &xf,float yf,float yi)
-{
-	float xm,ym;
-	
-	xm = (xi + xf)/2;
-	ym = (yi + yf)/2;
-	
-xi = xm ;
-	
- xf = ym ;
-	
-	
-	
-	
+ void PontoMedio ( float &xi,float &xf,float yf,float yi) { 
+	 
+	xi = (xi + xf)/2;
+	xf = (yi + yf)/2;
 }
 
 
-int main()
+int main() { 
 
-{
-	
-	
 	float xi,yi,xf,yf;
 	
-	cout<<"Digite o valor de xi\n";
-	cin>>xi;
+	cout << "Digite o valor de xi\n";
+	cin >> xi;
 	
 		
-	cout<<"Digite o valor de xf\n";
-	cin>>xf;
+	cout << "Digite o valor de xf\n";
+	cin >> xf;
 	
 		
-	cout<<"Digite o valor de  yf\n";
-	cin>>yf;
+	cout << "Digite o valor de  yf\n";
+	cin >> yf;
 	
 		
-	cout<<"Digite o valor de yi\n";
-	cin>>yi;
+	cout << "Digite o valor de yi\n";
+	cin >> yi;
 	
-	retorno(xi,xf,yf,yi);
-	cout<<xi<<" , "<<xf;
+	PontoMedio (xi,xf,yf,yi) ;
+	cout << xi << " , " << xf;
 	getch();
-	
-	
 }
