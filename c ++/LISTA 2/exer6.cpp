c36ff4262270a1584da1be853114bc30002f1432
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-12-05 10:44:37 -0300

    Update exer6.cpp

--- c ++/LISTA 2/exer6.cpp
+++ c ++/LISTA 2/exer6.cpp
@@ -1,44 +1,30 @@
 #include<iostream>
 #include<math.h>
-#include<conio.h>
+#include<conio.h>]
 
 using namespace std;
 
-
-float distancia(float xa,float xb,float ya,float yb)
-{
-	float p1;
-	
-	p1 = sqrt( pow(xa-xb,2) + pow(ya-yb,2));
-    return p1;
-	
-	
-	
+float DistanciaEntreDoisPontos(float xa,float xb,float ya,float yb) { 
+	return  sqrt( pow(xa-xb,2) + pow(ya-yb,2)); 
 }
 
-int main()
-{
-	float xa,xb,ya,yb;
-	float ret_distancia;
+int main() { 
+	float xa,xb,ya,yb , Retorno ;
 	
-	cout<<"DIGITE O VALOR DO PONTO XA ------->\n";
-	cin>>xa;
+	cout << "DIGITE O VALOR DO PONTO XA ------->\n";
+	cin >> xa;
 	
-		cout<<"DIGITE O VALOR DO PONTO XB ------->\n";
-	cin>>xb;
+	cout<<"DIGITE O VALOR DO PONTO XB ------->\n";
+	cin >> xb;
 	
-		cout<<"DIGITE O VALOR DO PONTO YA ------->\n";
-	cin>>ya;
+	cout<<"DIGITE O VALOR DO PONTO YA ------->\n";
+	cin >> ya;
 	
-		cout<<"DIGITE O VALOR DO PONTO YB ------->\n";
-	cin>>yb;
+	cout<<"DIGITE O VALOR DO PONTO YB ------->\n";
+	cin >> yb;
 	
-    ret_distancia = distancia(xa,xb,ya,yb);
+       Retorno = DistanciaEntreDoisPontos(xa,xb,ya,yb);
 	
-	cout<<"A distancia entre os dois pontos e : "<<ret_distancia;
+	cout << "A distancia entre os dois pontos e : " << Retorno ;
 	getch();
-	
-	
-	
-	
 }
