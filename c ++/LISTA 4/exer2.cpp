9fd62bbac646579305c240d09faf3e28d4d68c83
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-12-09 13:01:44 -0300

    Update exer2.cpp

--- c ++/LISTA 4/exer2.cpp
+++ c ++/LISTA 4/exer2.cpp
@@ -2,25 +2,23 @@
 #include<conio.h>
 using namespace std;
 
-bool fabs1(float n)
-{
-	bool retorno = false;
-  return  (n < 0 ) ? retorno =   true : retorno ;
+bool fabs1(float n) { 
+  return  (n < 0 ) ; 
 }
 
 
-int main()
-{
+int main() { 
+	
 	float n;
 	cout<<"Digite um valor\n";
-    cin>>n;
-    switch(fabs1(n))
-{
+    	cin>>n;
+    	switch(fabs1(n)) { 
+			
 	case true : cout << n * (-1);
 	break;
 	default :
-		cout << n;
-}
-    getch();
+	cout << n;
+	}
+    	getch();
 	
 }
