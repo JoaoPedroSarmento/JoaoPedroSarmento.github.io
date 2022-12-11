75458974e2c8eef5385a2e9e24a17096ae88761f
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-12-11 13:43:58 -0300

    Update exer16.cpp

--- c ++/LISTA 6/exer16.cpp
+++ c ++/LISTA 6/exer16.cpp
@@ -1,110 +1,27 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
 
-using namespace std ;
+using namespace std;
 
-int main () {
-
-	int l1 , c1 , l2 , c2 , tam = 8 ;
-
-	int x[tam] [tam] ;
-
-	int j = 1;
-
-	for ( int i = 0 ; i < tam ; i++ ) {
-		j = 1 ;
-		for ( int k = 0 ; k < tam ; k++) {
-			x[i] [k] = j ;
-            j++;
-		}
-	}
-
-    cout << "\nMatriz"<<endl ;
-
-	for ( int i = 0 ;  i < tam ; i++ ){
-		for ( int k = 0 ; k < tam ; k++) {
-			cout << x[i] [k] << "\t" ;
-		}
-		cout << endl ;
-	}
-
-	cout <<"\n";
-	cout << "XADREZ : " << endl ;
-	cout << "Insira a posicao inicial da peca bispo do xadrez -- > " ;
-	cout << "LINHA -- > ( max : 7 ) ( min : 0 ) " ;
-	cin  >> l1 ;
-	cout << "COLUNA -- > ( max : 7 ) ( min : 0 ) " ;
-	cin  >> c1 ;
-
-	cout << "Insira a posicao que voce deseja mexer na peca bispo do xadrez -- > ";
-    cout << "LINHA -- > ( max : 7 ) ( min : 0 ) " ;
-	cin  >> l2 ;
-	cout << "COLUNA -- > ( max : 7 ) ( min : 0 ) " ;
-    cin >> c2 ;
-
-
-   int Pc = 0;
-   int Pl = 0 ;
-
-   for ( int i = tam ; i > -1 ; i-- )  { // Posi��o das pe�as
-     if ( i == c2 ) {
-         while ( Pc < i ){
-             Pc++;
-         }
-        while(Pl <  l2 ) {
-            Pl++;
-        }
-     }
-   }
-
-     int cont = 0 ;
-
-     j = c1 ;
-
-     for ( int i =  l1  ; i <  tam  ; i++ ) { // Verifica diagonal principal
-         if ( i == Pl &&  j == Pc)  {
-            cont++;
-         }
-        j++;
-     }
-
-     j = c1;
-
-     for ( int i =  l1  ; i <  tam  ; i-- ) { // Verifica diagonal principal inversa
-
-         if ( i == Pl &&  j == Pc)  {
-            cont++;
-         }
-        j--;
-     }
-
-
-     j = c1;
-
-     for ( int i = l1    ; i >= 0 ; i-- ) { // Verifica diagonal secund�ria inversa , aumenta coluna e diminui a linha
-         if ( i == Pl &&  j == Pc)  {
-            cont++;
-         }
-        j++;
-    }
-
-
-        j = c1;
-        int i = l1 ;
-
-    for ( j   ; j >= 0 ; j-- ) { // Verifica diagonal secund�ria  , aumenta linha e diminui coluna
-         if ( i == Pl &&  j == Pc)  {
-            cont++;
-         }
-        i++;
-  }
-
-
-if ( cont == 1 ) {
-    cout << "Movimento possivel !!"  ;
+// Função para verificar se as posições de origem e destino são válidas
+// para um movimento de bispo
+bool podeMoverBispo(int xOrigem, int yOrigem, int xDestino, int yDestino)
+{
+  // Verificamos se a distância entre as coordenadas x é igual à distância entre as coordenadas y
+  // Isso é necessário, pois o bispo só pode se mover em diagonal
+  return (xOrigem - xDestino) == (yOrigem - yDestino);
 }
- else {
-    cout << "Movimento nao e possviel !! " ;
- }
 
+int main() { 
+   // Declare e inicialize as variaveis que armazenarao as posicoes do tabuleiro
+    int origem_x = 0, origem_y = 0, destino_x = 0, destino_y = 0;
+    cout << "Digite a posicao de origem: ";
+    cin >> origem_x >> origem_y;
+    cout << "Digite a posicao de destino: ";
+    cin >> destino_x >> destino_y;
+   
+   if ( podeMoverBispo )  cout << "E possivel mover o cavalo de (" << origem_x << "," << origem_y << ") para (" << destino_x << "," << destino_y << ")." << endl;
+   else 
+	        cout << "Nao e possivel mover o cavalo de (" << origem_x << "," << origem_y << ") para ("
+             << destino_x << "," << destino_y << ")." << endl;
+ 
 }
