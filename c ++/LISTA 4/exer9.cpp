4f9046a80a4696d79842dd1cc4a828770a0dd855
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2022-12-25 23:41:37 -0300

    Update exer9.cpp

--- c ++/LISTA 4/exer9.cpp
+++ c ++/LISTA 4/exer9.cpp
@@ -2,73 +2,57 @@
 #include<conio.h>
 using namespace std;
 
-int IndicaMes( int mes )
-{
-	int resultado = -1;
-( mes == 1)  ? resultado =  1 : resultado;
-( mes == 2)  ? resultado =  2 : resultado;
-( mes == 3)  ? resultado =  3 : resultado;
-( mes == 4)  ? resultado =  4 : resultado;
-( mes == 5)  ? resultado =  5 : resultado;
-( mes == 6)  ? resultado =  6 : resultado;
-( mes == 7)  ? resultado =  7 : resultado;
-( mes == 8)  ? resultado =  8 : resultado;
-( mes == 9)  ? resultado =  9 : resultado;
-( mes == 10) ? resultado =  10: resultado;
-( mes == 11) ? resultado =  11: resultado;
-( mes == 12) ? resultado =  12: resultado;
-return resultado;
+int IndicaMes( int mes ) { 
+return ( mes == 1)  ?  1 : ( mes == 2)  ?   2  : ( mes == 3)  ?  3 :  ( mes == 4)  ?  4  :
+       ( mes == 5)  ?  5 : ( mes == 6)  ?   6  : ( mes == 7)  ?  7 :  ( mes == 8)  ?  8  : 
+       ( mes == 9)  ?  9 : ( mes == 10) ?   10 : ( mes == 11) ?  11 : ( mes == 12) ?  12 : -1;
 }
 
-int main()
-{
-	int mes;
+int main() {
+    
+    int mes;
 	cout<<"Digite um mes de acordo com seu numero (1 a 12 )";
 	cin>>mes;
 
-	switch(IndicaMes(mes))
-	{
-	case 1:
+	switch(IndicaMes(mes)) { 
+	                case 1:
 			cout<<"JANEIRO";
 			break;
 			case 2 :
-				cout<<"FEVEREIRO";
-				break;
+			cout<<"FEVEREIRO";
+			break;
 			case 3:
 			cout<<"MARCO";
 			break;
 			case 4 :
-				cout<<"ABRIL";
-				break;
+			cout<<"ABRIL";
+			break;
 			case 5:
 			cout<<"MAIO";
 			break;
 			case 6 :
-				cout<<"JUNHO";
-				break;
+			cout<<"JUNHO";
+			break;
 			case 7:
 			cout<<"JULHO";
 			break;
-			
-					case 8:
+		        case 8:
 			cout<<"AGOSTO";
 			break;
 			case 9 :
-				cout<<"SETEMBRO";
-				break;
-					case 10:
+			cout<<"SETEMBRO";
+			break;
+			case 10:
 			cout<<"OUTUBRO";
 			break;
 			case 11 :
-				cout<<"NOVEMBRO";
-				break;
-		      case 12 : 
-			  cout<<"DEZEMBRO";
-			  break;
-			  default :
-			  
-			  cout<<"Isso nao e um mes ";
+			cout<<"NOVEMBRO";
+			break;
+		        case 12 : 
+			cout<<"DEZEMBRO";
+			break;
+			default :
+			cout<<"Isso nao e um mes ";
 		}
-		
 		getch();
 	}
