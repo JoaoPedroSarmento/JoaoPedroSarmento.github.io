ac16594d8dfa9f9e913eb81e321754d9d7334e33
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-02-02 12:09:57 -0300

    Update exer5.cpp

--- c ++/LISTA 5/exer5.cpp
+++ c ++/LISTA 5/exer5.cpp
@@ -3,29 +3,29 @@
 
 using namespace std;
 
-int fibonacci(int q)
-{
-   int cont = 1;
-  int anterior , atual, proximo;
+int fibonacci(int q) { 
   
+  int cont = 1;
+  int anterior , atual, proximo;
   anterior = 0;
   atual = 0;
   proximo = 1;	
-      	for(cont; cont <= q; cont++)
-	{
-	      cout<<"\t"<<atual<<endl;
-		proximo =  proximo + anterior;
-		anterior = atual;
-		atual = proximo;
-		
-	}
-}
-int  main()
-{   int q,resultado;
-	cout<<"Escreva a quantidade de numeros desejadas -->";
-	cin>>q;
-	fibonacci(q);
 	
+ 	for(cont; cont <= q; cont++) { 
+	     	 cout << "\t" << atual << endl;
+		 proximo =  proximo + anterior;
+		 anterior = atual;
+		 atual = proximo;
+		
+ 	}
+}
+int  main() { 
+	
+	int q,resultado;
+	cout << "Sequencia fibonacci \n";
+	cout << "Escreva a quantidade de numeros desejadas -->";
+	cin  >> q;
+	fibonacci(q);
 	getch();
 }
 
