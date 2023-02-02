1e6093e754c19256b51fbf26bbeb0cc335ec1437
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-02-02 11:52:36 -0300

    Update exer1.cpp

--- c ++/LISTA 4/exer1.cpp
+++ c ++/LISTA 4/exer1.cpp
@@ -3,32 +3,20 @@
 using namespace std;
 
 bool minuscula(char l) { 
-	
    return (l >= 'a' && l <= 'z' ) ; 
-	
-
 }
 
 bool maiuscula(char l) { 
-	
    return  (l >= 'A' && l <= 'Z' ) ;
-
-	
 }
+
 bool  letra (char l) {
-	
  return  (l >='A' && l <='Z' || l >='a' && l <='z' ) ;
-	
-	
 }
 
-int vogal(char l)
-{
-
+int vogal(char l) { 
     return ( ( l =='a' )  || ( l  == 'e' )  ||  ( l == 'i' )  || ( l =='o' )  || ( l =='u' )  || ( l =='A' )  || ( l == 'E' )  || ( l == 'I')  || (l =='O') 
 	|| (l =='U' )  ) ; 
-
-	
 }
 
 bool consoante(char l) { 
@@ -40,34 +28,19 @@ bool consoante(char l) {
 
 
 bool algarismo(char l) { 
-	
     return  (  l >= '1' && l <= '9'  );
-
-	
 }
 
-int main()
-{
+int main() { 
 	int t;
-char carac;
-cout<<"Digite um caractere \n";
-cin>>carac;
-(minuscula(carac)) ?  cout<<"Esse caractere e uma letra minuscula\n" : cout<<"";	
-
-(maiuscula(carac)) ? cout<<"Esse caractere e uma letra maiuscula\n" : cout<<"";
-
-(letra(carac)) ? cout<<"Esse caractere e uma letra\n" : cout<<"";
-
-	
-(vogal(carac)) ? cout<<"Esse caractere e uma vogal \n" : cout<<"";
-
-	
-(consoante(carac)) ? cout<<"Esse caractere e uma consoante \n" : cout<<"";
-
-	
-(algarismo(carac)) ? cout<<"Esse caractere e um algarismo \n" : cout<<"";
-
-getch();
-return 0 ;
-
+	char carac;
+	cout<<"Digite um caractere \n";
+	cin>>carac;
+	(minuscula(carac)) ?  cout<<"Esse caractere e uma letra minuscula\n" : cout<<"";	
+	(maiuscula(carac)) ? cout<<"Esse caractere e uma letra maiuscula\n" : cout<<"";
+ 	(letra(carac)) ? cout<<"Esse caractere e uma letra\n" : cout<<"";
+	(vogal(carac)) ? cout<<"Esse caractere e uma vogal \n" : cout<<"";
+	(consoante(carac)) ? cout<<"Esse caractere e uma consoante \n" : cout<<"";
+	(algarismo(carac)) ? cout<<"Esse caractere e um algarismo \n" : cout<<"";
+	getch();
 }
