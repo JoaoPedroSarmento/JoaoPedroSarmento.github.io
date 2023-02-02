5e869889bac853521cc3906ca3e901909f8badef
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-02-02 11:59:46 -0300

    Update exer6.cpp

--- c ++/LISTA 4/exer6.cpp
+++ c ++/LISTA 4/exer6.cpp
@@ -4,17 +4,12 @@ using namespace std;
 
 int imc(float peso,float altura ) { 
 	float IMC = peso/(altura*altura);
-	
 	return (IMC < 25) ?    0 : (IMC >= 25 && IMC < 30) ?   1 : (IMC >= 30 && IMC < 35)	?   2 :
-	(IMC >= 35 && IMC < 40) ?  3 : -1  ;
-	
-	
-	
+	(IMC >= 35 && IMC < 40) ?  3 : -1;
 }
 
-int main()
-
-{
+int main() { 
+	
 	float p,a;
 	int retorno;
 	cout<<"Digite seu peso :\n";
@@ -22,27 +17,23 @@ int main()
 	
 	cout<<"Digite sua altura: \n";
 	cin>>a;
-	
-
 	retorno = imc(p,a);
-	
 	switch(retorno) { 
 		case 0 :
-			cout << retorno << ", ausente de obesidade\n";
-			break;
-				case 1 :
-			cout << retorno << ", sobrepeso\n";
-			break;
-			case 2 :
-			cout << retorno << ", Obesidade grau I\n";
-			break;
-				case 3 :
-			cout << retorno << ", obesidade grau II\n";
-			break;
-		
-			break;
-			default:
-			cout << retorno << ", obesidade morbida\n";
+		cout << retorno << ", ausente de obesidade\n";
+		break;
+		case 1 :
+		cout << retorno << ", sobrepeso\n";
+		break;
+		case 2 :
+		cout << retorno << ", Obesidade grau I\n";
+		break;
+		case 3 :
+        	cout << retorno << ", obesidade grau II\n";
+		break;
+		default:
+		cout << retorno << ", obesidade morbida\n";
+		break;
 	}
 		getch();
 }
