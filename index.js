38a44720313646132f4fe4708a85d22c2cdbfaf7
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-02-13 18:04:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+function Projects() {
+    const SectionProject = document.getElementById(`projects`);
+    const Container2 = document.getElementById(`container-02`);
+    const Close = document.getElementById(`close`);
+    const BoxItens = document.createElement(`div`);
+    const DivItem1 = document.createElement(`div`);
+    const DivItem2 = document.createElement(`div`);
+    const DivItem3 = document.createElement(`div`);
+    const DivItem4 = document.createElement(`div`);
+    
+    Container2.style.display = `none`;
+    SectionProject.style.display = `block`;
+    Close.style.display = `block`;
+    
+    BoxItens.className = `Box-Itens`;
+    BoxItens.innerHTML = `
+    <div class = "box box1">
+    <div class="diagonal-plate">
+        <p>Sem Projetos</p>
+      </div> 
+    </div>
+
+    </div>
+    <div class = "box box2">
+     <div class="diagonal-plate">
+        <p>Sem Projetos</p>
+      </div>  
+    </div>  
+    <div class = "box box3">
+     <div class="diagonal-plate">
+        <p>Sem Projetos</p>
+      </div>  
+    </div>`
+
+    SectionProject.appendChild(BoxItens);
+    SectionProject.style.animation = `fadeIn   0.8s ease-in-out forwards`;   
+}
+
+function Close() {
+    const SectionProject = document.getElementById(`projects`);
+    const Container2 = document.getElementById(`container-02`);
+    const Close = document.getElementById(`close`);
+
+    const boxes = document.querySelectorAll(`.box`);
+    boxes.forEach(box => box.remove());
+
+    Container2.style.display = `flex`;
+    SectionProject.style.display = `none`;
+    Container2.style.animation = `slideInDown   0.8s ease-in-out forwards`;
+    Close.style.display = `none`;
+}
