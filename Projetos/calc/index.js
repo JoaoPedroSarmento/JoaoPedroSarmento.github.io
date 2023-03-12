94db65babaf63519ae41b100289c0a84d8f1b692
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-03-12 15:33:21 -0300

    Add files via upload

--- Projetos/calc/index.js
+++ Projetos/calc/index.js
@@ -1,8 +1,10 @@
 const Input = document.getElementById("input");
 const InputResult = document.getElementById("result");
 const BtnCopy = document.getElementById("Copy");
+const BtnMute = document.getElementById("Mute");
 const Root = document.querySelector(":root");
 const Main = document.querySelector("main");
+let audio = new Audio("./som/click.mp3");
 const AllowedsKeys = [
   " ",
   ".",
@@ -39,15 +41,16 @@ Input.addEventListener("keypress", function (ev) {
 document.getElementById("clear").addEventListener("click", function () {
   Input.value = "";
   Input.focus();
+  return Sound();
 });
 
 document.querySelectorAll(".Key").forEach(function (Button) {
   Button.addEventListener("click", function (valor) {
-    return (Input.value += Button.innerText);
+    return (Input.value += Button.innerText) + Sound();
   });
 });
 document.getElementById("igual").addEventListener("click", function () {
-  return Calculate();
+  return Calculate() + Sound();
 });
 BtnCopy.addEventListener("click", function () {
   return navigator.clipboard.writeText(InputResult.value);
@@ -80,3 +83,19 @@ function ThemeLight() {
 document.getElementById("Toggle").addEventListener("click", function () {
   return Main.dataset.theme === "Dark" ? ThemeLight() : ThemeDark();
 });
+
+function Sound() {
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function Mute() {
+  if (BtnMute.innerText === "Mutar") {
+    BtnMute.innerText = "Desmutar";
+    return (audio = new Audio("#"));
+  }
+  BtnMute.innerText = "Mutar";
+  return (audio = new Audio("./som/click.mp3"));
+}
+
+BtnMute.addEventListener("click", Mute);
