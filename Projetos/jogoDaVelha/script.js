d71d969cd4bf989eadfade0b7e5033e3735b35ae
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-03-30 21:14:57 -0300

    Add files via upload

--- Projetos/jogoDaVelha/script.js
+++ Projetos/jogoDaVelha/script.js
@@ -1,13 +1,7 @@
+let qtdDeJogador = [];
 const celulas = document.querySelectorAll(".cell");
-let pLastChild = document.getElementById("last-child");
-let jogadores = [];
-let posicoesO = [];
-let posicoesX = [];
-let jogador = "X";
-let vitoriasX = 0;
-let vitoriasO = 0;
-let celulasPreenchidas = 0;
-const vitoriasCombinacoes = [
+const pLastChild = document.getElementById("last-child");
+const combinacoesVitoria = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -17,82 +11,87 @@ const vitoriasCombinacoes = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-
+let posicoesX = [];
+let posicoesO = [];
+let vitoriasX = 0;
+let vitoriasO = 0;
+let contCliques = 0;
+let jogador = "X";
 function resetaJogo() {
-  celulas.forEach((celula) => (celula.textContent = ""));
   posicoesO = [];
   posicoesX = [];
-  jogador = "O";
-  celulasPreenchidas = 0;
+  celulas.forEach((celula) => (celula.textContent = ""));
+  contCliques = 0;
 }
 function adicionaJogador() {
-  const contJogadores = document.querySelectorAll(".player");
-  const inputJogador = document.getElementById("input");
   const section = document.getElementById("players");
-  const p = document.createElement("p");
+  const players = document.querySelectorAll(".player");
+  const input = document.getElementById("input").value;
+  const div = document.getElementById("game");
+  qtdDeJogador.push(input);
+  if (qtdDeJogador.length > 2) {
+    qtdDeJogador = [];
+    qtdDeJogador.push(input);
+    players.forEach((valor) => section.removeChild(valor));
+    resetaJogo();
+  }
+  const p = document.createElement("h3");
+  console.log(p);
+  p.textContent = `Jogador: ${input}`;
   p.setAttribute("class", "player");
-  if (contJogadores.length == 2) {
-    contJogadores.forEach((jogador) => section.removeChild(jogador));
-    pLastChild.textContent = "";
-    jogadores = [];
-  }
-  if (contJogadores.length == 1) {
-    p.innerText = `O(a) Jogador(a) ${inputJogador.value} é o O`;
-    jogadores.push(inputJogador.value);
-    const divGame = document.getElementById("game");
-    divGame.dataset.game = "true";
-    pLastChild.innerText = `Vez de ${jogadores[0]} jogar:`;
-  } else {
-    jogadores.push(inputJogador.value);
-    p.innerText = `O(a) Jogador(a) ${inputJogador.value} é o X`;
-  }
   section.append(p, pLastChild);
+  div.dataset.game = "false";
+  vitoriasX = 0;
+  vitoriasO = 0;
+  pLastChild.textContent = "";
+  if (comecaJogo()) {
+    div.dataset.game = "true";
+    pLastChild.textContent = `Vez de ${qtdDeJogador[0]}`;
+  }
 }
 function verificaVitoria() {
-  const players = document.querySelectorAll(".player");
-  vitoriasCombinacoes.forEach((combinacao) => {
-    if (combinacao.every((posicao) => posicoesO.includes(posicao))) {
-      vitoriasO += 1;
-      resetaJogo();
-      return true;
-    } else if (combinacao.every((posicao) => posicoesX.includes(posicao))) {
-      vitoriasX += 1;
-
-      resetaJogo();
-      return true;
-    } else if (celulasPreenchidas == 9) {
-      resetaJogo();
-    }
-    players[0].textContent = `O(a) Jogador(a) ${jogadores[0]} é o X. Vitórias: ${vitoriasX}`;
-    players[1].textContent = `O(a) Jogador(a) ${jogadores[1]} é o O. Vitórias: ${vitoriasO}`;
-  });
-  return false;
+  const h3 = document.querySelectorAll(".player");
+  if (
+    combinacoesVitoria.some((combinacao) =>
+      combinacao.every((valor) => posicoesX.includes(valor))
+    )
+  ) {
+    vitoriasX += 1;
+    resetaJogo();
+  } else if (
+    combinacoesVitoria.some((combinacao) =>
+      combinacao.every((valor) => posicoesO.includes(valor))
+    )
+  ) {
+    vitoriasO += 1;
+    resetaJogo();
+  } else if (contCliques === 9) {
+    alert("Empate");
+    resetaJogo();
+  }
+  h3[0].textContent = `Jogador: ${qtdDeJogador[0]}, vitórias ${vitoriasX}`;
+  h3[1].textContent = `Jogador: ${qtdDeJogador[1]}, vitórias ${vitoriasO}`;
 }
-function indicaJogador() {
-  return jogador === "X"
-    ? `Vez de ${jogadores[0]} jogar:`
-    : `Vez de ${jogadores[1]} jogar:`;
+function comecaJogo() {
+  return qtdDeJogador.length == 2;
 }
-celulas.forEach((celula, indice) => {
-  celula.addEventListener("click", (valor) => {
-    pLastChild.textContent = indicaJogador();
-    if (jogador === "O" && celula.textContent === "") {
-      jogador = "X";
-      console.log(pLastChild);
-      celula.textContent = "O";
-      posicoesO.push(indice);
-      celulasPreenchidas += 1;
-      console.log(celulasPreenchidas);
-    } else if (celula.textContent === "") {
-      jogador = "O";
-      celula.textContent = "X";
-      posicoesX.push(indice);
-      celulasPreenchidas += 1;
-      console.log(celulasPreenchidas);
-    }
-    pLastChild.textContent = indicaJogador();
-    verificaVitoria();
-  });
-});
-
+function jogada(element, indice) {
+  if (jogador === "X" && element.textContent === "") {
+    element.textContent = "X";
+    jogador = "O";
+    posicoesX.push(indice);
+    contCliques += 1;
+    pLastChild.textContent = `Vez de ${qtdDeJogador[1]}`;
+  } else if (element.textContent === "") {
+    element.textContent = "O";
+    jogador = "X";
+    posicoesO.push(indice);
+    contCliques += 1;
+    pLastChild.textContent = `Vez de ${qtdDeJogador[0]}`;
+  }
+  verificaVitoria();
+}
 document.getElementById("add").addEventListener("click", adicionaJogador);
+celulas.forEach((element, indice) => {
+  element.addEventListener("click", () => jogada(element, indice));
+});
