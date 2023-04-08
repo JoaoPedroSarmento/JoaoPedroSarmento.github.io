05162176f70e6ffed3ec4d022207c13b968a9cb7
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-04-07 22:00:28 -0300

    Add files via upload

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,8 +1,15 @@
 const menu = document.querySelector(".nav-bar div");
 menu.addEventListener("click", () => {
   const menuList = document.querySelector(".menu-list");
+  const header = document.querySelector("header");
+  const sections = document.querySelectorAll("section");
+  header.classList.toggle("menuToggle");
+  header.classList.toggle("menuToggleX");
   menu.classList.toggle("menu");
   menu.classList.toggle("menuX");
+  sections.forEach((section) => {
+    section.classList.toggle("reduce");
+  });
   menuList.dataset.display =
     menuList.dataset.display === "none" ? "grid" : "none";
 });
