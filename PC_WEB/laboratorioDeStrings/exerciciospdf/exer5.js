e5aef190e313921a359c6bc0ceb5d8243b24257d
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-04-17 19:00:06 -0300

    Update exer5.js

--- PC_WEB/laboratorioDeStrings/exerciciospdf/exer5.js
+++ PC_WEB/laboratorioDeStrings/exerciciospdf/exer5.js
@@ -1,25 +1,6 @@
-// Poderia ser feito com replace!!!
-function procurarPalavras(textoContainer, textoProcurado) {
-  textoContainer = textoContainer.split(" ");
-  return textoContainer.find((texto) => texto === textoProcurado);
-}
 function substitiuPalavra(textoContainer, textoParaSubtituir, textoProcurado) {
-  const arrayTextoProcurado = textoProcurado.split(" ");
-  textoParaSubtituir = textoParaSubtituir.split(" ");
-  let arrayTextos = textoContainer.split(" ");
-  for (let i = 0; i < arrayTextoProcurado.length; i += 1) {
-    let palavra = procurarPalavras(textoContainer, arrayTextoProcurado[i]);
-    if (palavra) {
-      for (let k = 0; k < arrayTextos.length; k += 1) {
-        if (palavra === arrayTextos[k]) {
-          arrayTextos[k] = textoParaSubtituir;
-        }
-      }
-    }
-  }
-  return arrayTextos.join(" ");
+  return textoContainer.split(textoProcurado).join(textoParaSubtituir);
 }
-
 document.getElementById("submit").addEventListener("click", () => {
   const p = document.getElementById("texto");
   const inputProcurar = document.getElementById("inputProcurar").value;
