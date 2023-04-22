dcd07abda801bb6cab3c40d2659335723e90aef1
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-04-22 11:37:23 -0300

    Update exer1.cpp

--- c ++/LISTA 4/exer1.cpp
+++ c ++/LISTA 4/exer1.cpp
@@ -1,46 +1,35 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-
-bool minuscula(char l) { 
-   return (l >= 'a' && l <= 'z' ) ; 
+bool minuscula(char carac){
+	return carac >= 'a' && carac <= 'z';
 }
-
-bool maiuscula(char l) { 
-   return  (l >= 'A' && l <= 'Z' ) ;
+bool maiuscula(char carac){
+	return carac >= 'A' && carac <= 'Z';
 }
-
-bool  letra (char l) {
- return  (l >='A' && l <='Z' || l >='a' && l <='z' ) ;
+bool letra(char carac){
+	return minuscula(carac) || maiuscula(carac);
 }
-
-int vogal(char l) { 
-    return ( ( l =='a' )  || ( l  == 'e' )  ||  ( l == 'i' )  || ( l =='o' )  || ( l =='u' )  || ( l =='A' )  || ( l == 'E' )  || ( l == 'I')  || (l =='O') 
-	|| (l =='U' )  ) ; 
+bool vogal(char carac){
+	return (carac == 'a') || (carac == 'e' )|| (carac == 'i') || (carac == 'o' )|| (carac == 'u') || 
+	(carac == 'A') || (carac == 'E') || (carac == 'I') || (carac == 'O' )|| (carac == 'U');
 }
-
-bool consoante(char l) { 
-    return  (  ( l != 'a')  &&  ( l  != 'A')  &&  ( l != 'e') &&  ( l != 'E') &&   
-            ( l != 'i')     &&  ( l != 'I')   &&  ( l != 'o')
-            &&  ( l != 'O') &&  ( l != 'u')   &&  ( l != 'U')
-            &&  !( l >= '1' && l <= '9' ));  
+bool consoante (char carac){
+	return letra(carac) && !(vogal(carac));
 }
-
-
-bool algarismo(char l) { 
-    return  (  l >= '1' && l <= '9'  );
+bool algarismo (char carac){
+	return carac >= '0' && carac <= '9';
 }
-
 int main() { 
 	int t;
 	char carac;
 	cout<<"Digite um caractere \n";
 	cin>>carac;
-	(minuscula(carac)) ? cout << "Esse caractere e uma letra minuscula\n" : cout << "";	
+	(minuscula(carac)) ? cout << "Esse caractere e uma letra minuscula\n" :
 	(maiuscula(carac)) ? cout <<"Esse caractere e uma letra maiuscula\n" : cout << "";
  	(letra(carac)) ?     cout <<"Esse caractere e uma letra\n" : cout << "";
-	(vogal(carac)) ?     cout <<"Esse caractere e uma vogal \n" : cout << "";
-	(consoante(carac)) ? cout <<"Esse caractere e uma consoante \n" : cout << "";
+	(vogal(carac)) ?     cout <<"Esse caractere e uma vogal \n" : 
+	(consoante(carac)) ? cout <<"Esse caractere e uma consoante \n" : 
 	(algarismo(carac)) ? cout <<"Esse caractere e um algarismo \n" : cout << "";
 	getch();
 }
