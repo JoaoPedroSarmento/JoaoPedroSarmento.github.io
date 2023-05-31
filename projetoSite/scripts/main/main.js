3338abf11661257ea76ccc8158eb41b8ae67e691
João Pedro Dieguez Moraes Sarmento <joaopedrosarmento12345@gmail.com>
2023-05-31 17:50:13 -0300

    Projeto site

---
+++ projetoSite/scripts/main/main.js
@@ -0,0 +1,3 @@
+import { removendoCapa } from "../removeCapa/removeCapa.js"; // Importando função para remover capa
+
+window.onload = removendoCapa(); // removendo a capa quando a página for carregada
---
+++ projetoSite/scripts/removeCapa/removeCapa.js
@@ -0,0 +1,15 @@
+export function removendoCapa() {
+  let tempo = 0;
+  const intervalo = setInterval(() => {
+    if (tempo === 1) {
+      const capa = document.querySelector("div.capa");
+      if (capa) {
+        // verifcando se a capa existe no documneto html para não gerar nenhum erro!
+        capa.remove(); // Removendo a capa do HTML
+      }
+      clearInterval(intervalo); // Parando com o loop
+    }
+    tempo += 1;
+  }, 0); // a cada 1 segundo executa o código passado
+}
+// função exportada!!
