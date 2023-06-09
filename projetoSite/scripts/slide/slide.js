23bfae390507668b0f990daaeaeb3fed194e2ed0
João Pedro Dieguez Moraes Sarmento <joaopedrosarmento12345@gmail.com>
2023-06-09 12:39:06 -0300

    Projeto site

--- projetoSite/scripts/slide/slide.js
+++ projetoSite/scripts/slide/slide.js
@@ -1,16 +1,17 @@
-// melhorar o código!!
-
+// melhorar o código e o nome das variáveis!!
+// pega o índice
 let indiceAtual = 0;
 export function rodaSlides() {
   const bolas = document.querySelectorAll(".bola");
   trocaSlidesComSeta();
+  trocaSlidesComTouchMobile();
+  trocaSlidesComCLique();
   bolas.forEach((bola, indice) => {
     bola.addEventListener("click", () => {
       trocaSlides(indice);
     });
   });
 }
-
 function trocaSlides(indice) {
   const bolas = document.querySelectorAll(".bola");
   const slides = document.querySelectorAll(".slide");
@@ -36,16 +37,48 @@ function trocaSlidesComSeta() {
     const slides = document.querySelectorAll(".slide");
     if (ev.key === "ArrowLeft" || ev.key === "a") {
       indiceAtual -= 1;
-      if (indiceAtual < 0) {
-        indiceAtual = slides.length - 1;
-      }
+      verificaIndice();
       trocaSlides(indiceAtual);
     } else if (ev.key === "ArrowRight" || ev.key === "d") {
       indiceAtual += 1;
-      if (indiceAtual >= slides.length) {
-        indiceAtual = 0;
-      }
+      verificaIndice();
       trocaSlides(indiceAtual);
     }
   });
+}
+
+function trocaSlidesComTouchMobile() {
+  let startX = 0;
+  window.addEventListener("touchstart", (event) => {
+    startX = event.touches[0].clientX;
+  });
+
+  window.addEventListener("touchmove", (event) => {
+    const currentX = event.touches[0].clientX;
+    if (currentX > startX) {
+      indiceAtual += 1;
+      verificaIndice();
+    } else if (currentX < startX) {
+      indiceAtual -= 1;
+      verificaIndice();
+    }
+    trocaSlides(indiceAtual);
+    startX = currentX;
+  });
+}
+
+function trocaSlidesComCLique() {
+  document.getElementById("planos").addEventListener("click", () => {
+    indiceAtual += 1;
+    verificaIndice();
+    trocaSlides(indiceAtual);
+  });
+}
+function verificaIndice() {
+  const slides = document.querySelectorAll(".slide");
+  if (indiceAtual < 0) {
+    indiceAtual = slides.length - 1;
+  } else if (indiceAtual >= slides.length) {
+    indiceAtual = 0;
+  }
 }
