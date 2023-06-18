8702b05f6bce356f314c7e2e48d7ff841927faee
João Pedro Dieguez Moraes Sarmento <joaopedrosarmento12345@gmail.com>
2023-06-18 15:55:26 -0300

    Projeto site

--- site/scripts/header.js
+++ site/scripts/header.js
@@ -1,6 +1,6 @@
 function ativaHeader() {
   const header = document.querySelector("header");
-  if (window.scrollY > 20) {
+  if (window.scrollY > 10) {
     header.classList.add("active");
   } else {
     header.classList.remove("active");
