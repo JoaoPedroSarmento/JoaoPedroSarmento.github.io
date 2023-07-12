db4e9d82c682abc6a8547ca24574d3bd88eed55e
João Pedro Dieguez Moraes Sarmento <joaopedrosarmento12345@gmail.com>
2023-07-11 21:25:47 -0300

    commit all

--- js/topicos-avancados/await/await.js
+++ js/topicos-avancados/await/await.js
@@ -15,3 +15,34 @@ async function execute() {
 }
 
 execute();
+
+
+
+async function soma2(...numeros) {
+    if (numeros.find((valor) => typeof valor != "number")) {
+        return Promise.reject("Um valor inserido não é um número")
+    }
+    return numeros.reduce((acum, valor) => acum += valor, 0);
+}
+
+
+async function execute2() {
+    try {
+        // await == aguardem
+        const resultado = await soma2(3, 4, 5, 6);
+        console.log(resultado);
+    } catch (erro) {
+        console.log("Deu ruim ");
+    }
+}
+execute2();
+// async function execute() {
+//     const resultado = await soma(3, 4, 5, 6, 7, 10, "ddd");
+//     console.log(resultado);
+// }
+
+// execute().then((resultado) => {
+//     console.log("Resultado obtido" + resultado);
+// }).catch((erro) => {
+//     console.log("Ouve um erro")
+// })
