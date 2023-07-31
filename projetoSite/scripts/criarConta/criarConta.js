5cefdb23e6e6af577d60dc8b2a4c815efff0ad1b
João Pedro Dieguez Moraes Sarmento <103864422+JoaoPedroSarmento@users.noreply.github.com>
2023-07-30 21:38:57 -0300

    delete projetoSite

--- projetoSite/scripts/criarConta/criarConta.js
+++
@@ -1,7 +0,0 @@
-import { removecapa } from "../removeCapa/removeCapa.js"; // Importando função para remover capa
-import { mostraSenha } from "../login/login.js";
-window.onload = removecapa(); // removendo a capa quando a página for carregada
-
-document
-  .querySelector(".input-password .fa-eye")
-  .addEventListener("click", mostraSenha);
